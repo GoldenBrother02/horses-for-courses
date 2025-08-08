80bb28200c3a2489f864f600a74d6504655f6357
GoldenBrother02 <marcke2003@gmail.com>
2025-08-08 10:00:44 +0200

    CRUDTests

---
+++ HorsesForCourses.Tests/API/CoachCRUDTest.cs
@@ -0,0 +1,33 @@
+using HorsesForCourses.Core;
+using HorsesForCourses.Tests;
+using HorsesForCourses.WebApi;
+using Microsoft.EntityFrameworkCore;
+
+namespace HorsesForCourses.Tests;
+
+public class CoachCrudTests : CrudTestBase<AppDbContext, Coach>
+{
+    protected override AppDbContext CreateContext(DbContextOptions<AppDbContext> options) => new AppDbContext(options);
+
+    protected override Coach CreateEntity()
+        => new Coach(1, "Unit Test", "unit@test.com");
+
+    protected override DbSet<Coach> GetDbSet(AppDbContext context)
+        => context.Coaches;
+
+    protected override object[] GetPrimaryKey(Coach entity)
+        => new object[] { entity.Id };
+
+    protected override async Task ModifyEntityAsync(Coach entity)
+    {
+        entity.OverwriteCompetenties(new List<string> { "unit", "test" });
+        await Task.CompletedTask;
+    }
+
+    protected override async Task AssertUpdatedAsync(Coach entity)
+    {
+        Assert.Contains("unit", entity.competencies);
+        Assert.Contains("test", entity.competencies);
+        await Task.CompletedTask;
+    }
+}
---
+++ HorsesForCourses.Tests/API/CourseCRUDTest.cs
@@ -0,0 +1,30 @@
+using HorsesForCourses.Core;
+using HorsesForCourses.Tests;
+using HorsesForCourses.WebApi;
+using Microsoft.EntityFrameworkCore;
+
+namespace HorsesForCourses.Tests;
+
+public class CourseCrudTests : CrudTestBase<AppDbContext, Course>
+{
+    protected override AppDbContext CreateContext(DbContextOptions<AppDbContext> options) => new AppDbContext(options);
+
+    protected override Course CreateEntity() => new Course(1, "test", new DateOnly(2025, 6, 6), new DateOnly(2026, 6, 6));
+
+    protected override DbSet<Course> GetDbSet(AppDbContext context) => context.Courses;
+
+    protected override object[] GetPrimaryKey(Course entity) => new object[] { entity.Id };
+
+    protected override async Task ModifyEntityAsync(Course entity)
+    {
+        entity.OverwriteRequirements(new List<string> { "unit", "test" });
+        await Task.CompletedTask;
+    }
+
+    protected override async Task AssertUpdatedAsync(Course entity)
+    {
+        Assert.Contains("unit", entity.RequiredCompetencies);
+        Assert.Contains("test", entity.RequiredCompetencies);
+        await Task.CompletedTask;
+    }
+}
